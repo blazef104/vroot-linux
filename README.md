92e0e5b4cfc3c1d2df554dd589dab397ae741141
blaze <nanc23@rocketmail.com>
2017-12-12 23:21:34 +0100

    added snort

--- README.md
+++ README.md
@@ -6,7 +6,8 @@ This image is based on [Phusion](http://www.phusion.nl/) baseimage-docker and sl
 You can use it as a base for your own Docker images. For more info on this
 please check the [basedocker-image](https://github.com/phusion/baseimage-docker) README or the source code.
 
+*This image is modified to have a working instance of snort inside imunes. Use to command __ldconfig__ on the machine before launching snort.* 
+
 -----------------------------------------
 
 [<img src="http://imunes.tel.fer.hr/images/imunes_logo.png">](http://www.imunes.net/)
-
--- image/utilities.sh
+++ image/utilities.sh
@@ -7,4 +7,32 @@ set -x
 $minimal_apt_get_install curl less nano vim psmisc tcpdump iputils-ping \
 iputils-arping iputils-tracepath net-tools file telnet isc-dhcp-client nmap \
 dnsutils netcat man dsniff traceroute ettercap-text-only tcpreplay hping3 p0f \
-lsof strace elinks iperf iftop scapy bsd-mailx ndisc6 radvd iptables tayga ftp
+lsof strace elinks iperf iftop scapy bsd-mailx ndisc6 radvd iptables tayga ftp \
+flex bison g++ wget libpcap-dev make libpcre3-dev zlib1g-dev
+
+wget https://www.snort.org/downloads/snort/daq-2.0.6.tar.gz --no-check-certificate
+wget https://www.snort.org/downloads/snort/snort-2.9.11.tar.gz --no-check-certificate
+wget http://prdownloads.sourceforge.net/libdnet/libdnet-1.11.tar.gz --no-check-certificate
+
+tar xvfz daq-2.0.6.tar.gz
+cd daq-2.0.6
+./configure && make && make install
+cd ..
+rm daq-2.0.6.tar.gz
+rm -rf daq-2.0.6
+
+tar xvfz libdnet-1.11.tar.gz
+cd libdnet-1.11
+./configure && make && make install
+cd ..
+rm libdnet-1.11.tar.gz
+rm -rf libdnet-1.11.tar.gz
+
+tar xvfz snort-2.9.11.tar.gz
+cd snort-2.9.11
+./configure --enable-sourcefire && make && make install
+cd ..
+rm snort-2.9.11.tar.gz
+rm -rf snort-2.9.11
+
+ln -s /usr/local/lib/libdnet.1.0.1 /usr/lib/libdnet.1
